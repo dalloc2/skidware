22623f8c63089d9eeb045dea6bf309a8481e8b26
dalloc2 <128258555+dalloc2@users.noreply.github.com>
2023-09-30 21:34:49 +0100

    Create GunStates.lua

---
+++ GunStates.lua
@@ -0,0 +1,57 @@
+return {
+	["M9"] = {
+		MaxAmmo = 15,
+		CurrentAmmo = 15,
+		FireRate = 0.08,
+		AutoFire = false,
+		Range = 600,
+		Spread = 11,
+		ReloadTime = 2,
+		Bullets = 1,
+	},
+
+	["Remington 870"] = {
+		MaxAmmo = 6,
+		CurrentAmmo = 6,
+		StoredAmmo = 600,
+		FireRate = 0.8,
+		AutoFire = false,
+		Range = 400,
+		Spread = 3,
+		ReloadTime = 4,
+		Bullets = 5,
+	},
+
+	["AK-47"] = {
+		MaxAmmo = 30,
+		CurrentAmmo = 30,
+		FireRate = 0.1,
+		AutoFire = true,
+		Range = 800,
+		Spread = 14,
+		ReloadTime = 2,
+		Bullets = 1,
+	},
+
+	["M4A1"] = {
+		MaxAmmo = 30,
+		CurrentAmmo = 30,
+		FireRate = 0.0875,
+		AutoFire = true,
+		Range = 800,
+		Spread = 18,
+		ReloadTime = 2,
+		Bullets = 1,
+	},
+
+	["FAL"] = {
+		MaxAmmo = 20,
+		CurrentAmmo = 20, 
+		FireRate = 0.08, 
+		AutoFire = true, 
+		Range = 1000, 
+		Spread = 12, 
+		ReloadTime = 2, 
+		Bullets = 1, 
+	}
+}
